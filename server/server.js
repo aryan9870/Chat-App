9b440884865f77368047fdec27b9a7f828ce482a
Aryan <aryan7017n@gmail.com>
2026-08-06 12:16:20 +0530

    connect database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 // App
 const app = express();
@@ -20,7 +28,7 @@ app.get("/", (req, res) => {
 });
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
