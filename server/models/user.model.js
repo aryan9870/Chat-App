d35da471889dc969430f7011421403b7b99b0ba3
Aryan <aryan7017n@gmail.com>
2026-08-06 12:27:04 +0530

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+            default: "",
+        },
+        bio: {
+            type: String,
+            default: "Hey there! I am using Chat App.",
+        },
+    },
+    { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
