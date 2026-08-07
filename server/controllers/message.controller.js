26ef1ed149d6286ff59dc9881b587f36d1ec246e
Aryan <aryan7017n@gmail.com>
2026-08-07 13:51:13 +0530

    Implement messaging functionality

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -3,42 +3,59 @@ import Message from "../models/message.model.js";
 
 // Get all users for sidebar
 export const getUsersForSidebar = async (req, res) => {
-    // TODO: Get all users except logged-in user
+
+    // 1. Get logged-in user id from req.user
+    const loggedInUser = req.user._id;
+    // 2. Fetch all users except the logged-in user
+    const users = await User.find({ _id: { $ne: loggedInUser } }).select("-password");
 
     res.status(200).json({
       success: true,
-      users: [],
+      users: users,
     });
     
 };
 
 // Get conversation with selected user
 export const getMessages = async (req, res) => {
-    // TODO:
+
     // 1. Get selected user id from req.params.id
+    const selectedUserId = req.params.id;
     // 2. Get logged-in user id from req.user
+    const loggedInUserId = req.user._id;
     // 3. Fetch all messages between both users
-    // 4. Return messages
+    const messages = await Message.find({
+      $or: [
+        { sender: loggedInUserId, receiver: selectedUserId },
+        { sender: selectedUserId, receiver: loggedInUserId },
+      ],
+    }).sort({ createdAt: 1 });
 
     res.status(200).json({
       success: true,
-      messages: [],
+      messages: messages,
     });
   
 };
 
 // Send message
 export const sendMessage = async (req, res) => {
-    // TODO:
+
     // 1. Get receiver id from req.params.id
+    const receiverId = req.params.id;
     // 2. Get sender id from req.user
+    const senderId = req.user._id;
     // 3. Get content from req.body
+    const content = req.body.content;
     // 4. Create new message
-    // 5. Save message
-    // 6. Return saved message
+    const newMessage = await Message.create({
+      sender: senderId,
+      receiver: receiverId,
+      content: content,
+    });
 
     res.status(201).json({
       success: true,
-      message: {},
+      message: newMessage,
     });
 };
